c16a1614a77b43f4c257a65366a83222d4938c6c
jugovicm <121815082+jugovicm@users.noreply.github.com>
2023-05-12 13:50:14 +0200

    Delete Student Feature
    - Implement Delete Student Feature

--- src/main/java/com/jugovicm/studentmanagmentsystem/controller/StudentController.java
+++ src/main/java/com/jugovicm/studentmanagmentsystem/controller/StudentController.java
@@ -4,10 +4,7 @@ import com.jugovicm.studentmanagmentsystem.entity.Student;
 import com.jugovicm.studentmanagmentsystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -56,5 +53,11 @@ public class StudentController {
             studentService.updateStudent ( existingStudent );
             return "redirect:/students";
         }
+
+    @GetMapping ("/students/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        studentService.deleteStudentById ( id );
+        return "redirect:/students";
+    }
 
 }
